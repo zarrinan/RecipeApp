eabaced58d5fddfc26d9e0f13995e175c03a8fff
Zarrina Niyazova <zarrina_@hotmail.com>
2017-09-30 14:21:49 -0400

    adds ingredients controller, index view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,8 +5,10 @@ class CoursesController < ApplicationController
 
   def show
     @course = Course.find(params[:id])
-    @name = @course.name
-    @time = @course.time_of_day
-    @recipe = @course.recipes
+    @brf_recipes = Recipe.where(course_id: 1)
+    @lunch_recipes = Recipe.where(course_id: 2)
+    @dinner_recipes = Recipe.where(course_id: 3)
+    @dessert_recipes = Recipe.where(course_id: 4)
+
   end
 end
