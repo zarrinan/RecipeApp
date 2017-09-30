92c528de13b915173581e8902c634989898cca14
Zarrina Niyazova <zarrina_@hotmail.com>
2017-09-30 12:00:45 -0400

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcomes/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :welcomes, only: [:index]
+resources :courses, only: [:index, :show]
+resources :recipes, only: [:index, :show]
+
 end
