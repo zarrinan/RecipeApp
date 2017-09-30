a7df4087d716d0f02557f76340ef2dc0e077c44d
Zarrina Niyazova <zarrina_@hotmail.com>
2017-09-30 12:46:55 -0400

    adds recipe controller, index, show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'recipes/index'
+
+  get 'recipes/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :welcomes, only: [:index]
 resources :courses, only: [:index, :show]
