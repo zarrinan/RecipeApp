1965c88a5ab88087345ccc400d20019619125975
Zarrina Niyazova <zarrina_@hotmail.com>
2017-09-30 14:22:46 -0400

    adds ingredients route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'recipes/index'
-
-  get 'recipes/show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :welcomes, only: [:index]
 resources :courses, only: [:index, :show]
 resources :recipes, only: [:index, :show]
+resources :ingredients, only: [:index]
+
 
 end
